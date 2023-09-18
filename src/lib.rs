de350a0c88bbec76ae0e5cd9be3964a348aff6b7
Benny Peckruhn <benny@bennyville.de>
2023-09-18 05:59:32 +0200

    add files

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
